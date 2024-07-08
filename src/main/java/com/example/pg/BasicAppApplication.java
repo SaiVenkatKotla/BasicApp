6fb99f8394df4944c0ce9a62730a36949d2d0980
saivenkat.kotla <saivenkat.kotla@ivycomptech.com>
2024-07-08 18:27:13 +0530

    Initial commit

---
+++ src/main/java/com/example/pg/BasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pg/BasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
